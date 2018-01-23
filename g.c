a61d12abd2aec1be8353aea69267e0b8f167a2f0
KISHOREMURUGAN <35652455+KISHOREMURUGAN@users.noreply.github.com>
2018-01-23 08:23:17 -0800

    Create g.c

---
+++ g.c
@@ -0,0 +1,1 @@
+#include<stdio.h> #include<conio.h> void main() { int a; clrscr(); printf("Enter a number"); scanf("%d",&a); if (a=0) printf("The number is zero"); else if (a>1) printf("The number is positive"); else (a<0) printf("The number is negative"); getch(); }
